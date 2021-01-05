968ca1c6d2ed5076a2a34e175a3177fef2dd2a1d
CristhianNina <44449380+cristhiannina@users.noreply.github.com>
2021-01-05 14:47:33 -0400

    importando datos desde Firestore de la lista de categorías. CategoryActivity y CategoryAdapter

--- app/src/main/java/com/example/quizadmin/CategoryActivity.java
+++ app/src/main/java/com/example/quizadmin/CategoryActivity.java
@@ -2,22 +2,36 @@ package com.example.quizadmin;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 import java.util.List;
-
 
 
 public class CategoryActivity extends AppCompatActivity {
 
     private RecyclerView cat_recycler_view;
     private Button addCatB;
+    public static List<String> catList = new ArrayList<>();
+    private FirebaseFirestore firestore;
+    private Dialog loadingDialog;
+
 
 
 
@@ -26,31 +40,80 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-      Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Categories");
 
         cat_recycler_view = findViewById(R.id.cat_recycler);
         addCatB = findViewById(R.id.addCatB);
 
+        loadingDialog = new Dialog(CategoryActivity.this);
+        loadingDialog.setContentView(R.layout.loading_progressbar);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progress_background);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        List<String> catList = new ArrayList<>();
-        catList.add("CAT 1");
-        catList.add("CAT 2");
-        catList.add("CAT 3");
-        catList.add("CAT 4");
+        firestore = FirebaseFirestore.getInstance();
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         cat_recycler_view.setLayoutManager(layoutManager);
 
-
-        CategoryAdapter adapter = new CategoryAdapter(catList);
-        cat_recycler_view.setAdapter(adapter);
-
+        loadData();
 
     }
 
+    private void loadData()
+    {
 
+        loadingDialog.show();
+
+        catList.clear();
+
+        firestore.collection("QUIZ").document("Categories")
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                if(task.isSuccessful())
+                {
+                    DocumentSnapshot doc = task.getResult();
+
+                    if(doc.exists())
+                    {
+                        long count = (long)doc.get("COUNT");
+
+                        for(int i=1; i <= count; i++)
+                        {
+                            String catName = doc.getString("CAT" + String.valueOf(i));
+
+                            catList.add(catName);
+                        }
+
+                        CategoryAdapter adapter = new CategoryAdapter(catList);
+                        cat_recycler_view.setAdapter(adapter);
+
+                    }
+                    else
+                    {
+                        Toast.makeText(CategoryActivity.this,"No Category Document Exists!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                }
+                else
+                {
+
+                    Toast.makeText(CategoryActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+
+                loadingDialog.dismiss();
+
+            }
+        });
+
+    }
 
 }
+
+
